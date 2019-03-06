494fc1d8a25228d3e578013cea1723e3879e8c68
Your Name <your.email@example.com>
2019-03-06 02:46:52 +0000

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :microposts
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # see http://railsguides.jp/routing.html
   root 'application#hello'
